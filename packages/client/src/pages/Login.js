421c7341f447a6101961f1ea4e8e12e91b393bee
Nima <nima@movio.co>
2018-02-08 16:04:41 +1300

    Client Login page

---
+++ packages/client/src/pages/Login.js
@@ -0,0 +1,25 @@
+import React, {Component, Fragment} from 'react';
+import PropTypes from 'prop-types';
+import authProvider from "../auth/authProvider";
+import {CircularProgress} from 'material-ui/Progress';
+
+class Login extends Component {
+    componentDidMount() {
+        authProvider.login();
+    }
+
+    render() {
+        return (
+            <Fragment>
+                Redirecting to login page ...
+                <CircularProgress/>
+            </Fragment>
+        );
+    }
+}
+
+Login.propTypes = {
+
+};
+
+export default Login;
