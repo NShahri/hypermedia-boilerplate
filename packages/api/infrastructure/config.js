59d6c566f33a81176c54e3b7f9b9c654d82bee56
Nima <nima@movio.co>
2018-02-13 09:40:35 +1300

    Fix auth config

--- packages/api/infrastructure/config.js
+++ packages/api/infrastructure/config.js
@@ -3,7 +3,7 @@ export default {
     logLevel: 'trace',
 
     serverPort: 8080,
-    serverBaseUrl: 'http://localhost:8080',
+    serverBaseUrl: 'http://localhost:8080/',
 
     isProduction: process.env.NODE_ENV === 'production'
 }
